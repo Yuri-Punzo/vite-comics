24f844008c7d1fedc2add8d41d9d351dd4addf7c
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-10 18:05:07 +0100

    moved ex description, progress in footer

--- src/components/SiteFooterCenter.vue
+++ src/components/SiteFooterCenter.vue
@@ -5,9 +5,62 @@ export default {
 </script>
 
 <template>
-    <p>sono footer center</p>
+    <div id="site_footer_center">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto my_col">
+                    <h2>DC COMICS</h2>
+                    <a href="">Characters</a>
+                    <a href="">Comics</a>
+                    <a href="">Movies</a>
+                    <a href="">TV</a>
+                    <a href="">Games</a>
+                    <a href="">Videos</a>
+                    <a href="">News</a>
+                    <h2>SHOP</h2>
+                    <a href="">Shop DC</a>
+                    <a href="">Shop DC Collectibles</a>
+                </div>
+                <div class="col-auto my_col">
+                    <h2>DC</h2>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+
+                </div>
+                <div class="col-auto my_col">
+                    <h2>SITES</h2>
+                    <a href="">DC</a>
+                    <a href="">MAD Magazine</a>
+                    <a href="">DC Kids</a>
+                    <a href="">DC Universe</a>
+                    <a href="">DC Power Visa</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.my_col {
+    a {
+        color: gray;
+        font-size: 8;
+        display: block;
+        text-decoration: none;
 
+        &:hover {
+            color: #0282f9;
+        }
+    }
+}
 </style>
